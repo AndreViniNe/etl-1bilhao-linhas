70b115b4a137a9708a196dbab139ca88ece8381f
Andre Vinicius <andrevini.neves@gmail.com>
2024-07-16 23:36:45 -0300

    etl-pandas

--- etl-pandas.py
+++ etl-pandas.py
@@ -1,1 +1,17 @@
 import pandas as pd
+
+df = pd.read_csv("data/measurements.txt", 
+                 sep=";",
+                 header=None,
+                 names=['station', 'measure']
+            )
+
+print(df)
+df_agg = df.groupby("station")
+df_kpi = df_agg['measure'].agg({
+        'min': 'min',
+        'mean': 'mean',
+        'max': 'max'
+    })
+
+df_sort = df_kpi.sort_values('station')
